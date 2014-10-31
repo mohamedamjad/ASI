636cdf2f0ab1d61c1dc5df60b3f74265dd93d154
Mohamed Amjad LASRI <mohamed-amjad.lasri@ensg.eu>
2014-10-31 12:40:38 +0100

    Create run_me.sh
    Création du fichier run_me.sh (ce fichier crée l'environnement nécéssaire aux containers)

---
+++ run_me.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "Creating boot2docker repository"
+mkdir boot2docker
+cd boot2docker
+echo "Done !"
+
+echo "Initializing the virtual box ..."
+vagrant init dduportal/boot2docker
+echo "Done !"
+
+echo "Configuring the virtual box"
+vagrant box add dduportal/boot2docker ../boot2docker_virtualbox_1.2.0.box
+#https://vagrantcloud.com/dduportal/boxes/boot2docker/versions/11/providers/virtualb$
+echo "Done !"
+
+echo "Starting the virtual box"
+vagrant up
+sleep 10
+vagrant ssh
+echo "Virtual box closed !"
