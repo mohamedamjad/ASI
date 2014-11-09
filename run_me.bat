15e2f54f1b8fbf4335597ad23f9717f4e68666bd
Mohamed Amjad LASRI <amjad.sig@gmail.com>
2014-11-09 13:04:15 +0100

    Add the data directory it will contains the configured Vagrantfile to forward the 8081 port

--- run_me.bat
+++ run_me.bat
@@ -1,6 +1,6 @@
 :: This file is the equivalent of the run_me.sh script in Windows
 
-echo "Creating boot2docker repository"
+echo "Creating boot2docker repository ..."
 mkdir boot2docker
 cd boot2docker
 echo "Done !"
@@ -9,12 +9,17 @@ echo "Initializing the virtual box ..."
 vagrant init dduportal/boot2docker
 echo "Done !"
 
-echo "Configuring the virtual box"
+echo "Configuring the virtual box ..."
 vagrant box add dduportal/boot2docker ../boot2docker_virtualbox_1.2.0.box
 #https://vagrantcloud.com/dduportal/boxes/boot2docker/versions/11/providers/virtualb$
 echo "Done !"
 
-echo "Starting the virtual box"
+echo "Configuring the Vagrantfile ..."
+del Vagrantfile
+xcopy /f ..\data\Vagrantfile
+echo "Done !"
+
+echo "Starting the virtual box ..."
 vagrant up
 sleep 10
 vagrant ssh
--- run_me.sh
+++ run_me.sh
@@ -13,6 +13,11 @@ vagrant box add dduportal/boot2docker ../boot2docker_virtualbox_1.2.0.box
 #https://vagrantcloud.com/dduportal/boxes/boot2docker/versions/11/providers/virtualb$
 echo "Done !"
 
+echo "Configuring the Vagrantfile ..."
+rm Vagrantfile
+cp  ../data/Vagrantfile ../boot2docker
+echo "Done !"
+
 echo "Starting the virtual box"
 vagrant up
 sleep 10
